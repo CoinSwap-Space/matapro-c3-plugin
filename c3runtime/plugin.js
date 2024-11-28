103786eb3ef4c390585db0fd5f3d4ef95be3f1c8
Jakub Antczak <jakubantczak.info@gmail.com>
2024-11-28 13:25:22 +0100

    MPRO-001: Updates version number and improves code formatting
    Bumps the plugin version from 1.4.0.0 to 1.4.0.2 for release preparations.
    
    Refines code formatting and consistency for better readability and maintainability, including updated indentation and line spacing in several scripts.
    
    No functional changes were made.

--- c3runtime/plugin.js
+++ c3runtime/plugin.js
@@ -1,17 +1,15 @@
+const C3 = self.C3
 
-const C3 = self.C3;
+const DOM_COMPONENT_ID = 'MetaproPlugin'
 
-const DOM_COMPONENT_ID = "MetaproPlugin";
-
-C3.Plugins.MetaproPlugin = class MetaproPluginPlugin extends C3.SDKDOMPluginBase
-{
-	constructor(opts)
-	{
-		super(opts, DOM_COMPONENT_ID);
+C3.Plugins.MetaproPlugin = class MetaproPluginPlugin extends (
+	C3.SDKDOMPluginBase
+) {
+	constructor(opts) {
+		super(opts, DOM_COMPONENT_ID)
 	}
-	
-	Release()
-	{
-		super.Release();
+
+	Release() {
+		super.Release()
 	}
-}; 
+}
